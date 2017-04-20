c822940a6e2202ac19c9631c19b607974d3b068c
jonghoon <jhwgnu@hanmail.net>
2017-04-20 16:17:12 +0900

    add examples

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns = [
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+]
